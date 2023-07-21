446628cf9dd74a2e064f3a40d73b099ee2e0af52
PRATIKLAHANE-77 <pratik.lahane1999@gmail.com>
2023-07-21 21:03:35 +0530

    stored the informtaion in local storage

---
+++ index.js
@@ -0,0 +1,19 @@
+// localStorage.setItem('name','pratik')
+// localStorage.removeItem('name');
+// // console.log(localStorage.getItem('name'))
+// // console.log(localStorage.removeItem('name'));
+
+// //session storage
+// sessionStorage.setItem('name', 'pratik')
+// // console.log(sessionStorage.getItem('name'))
+// sessionStorage.removeItem('name');
+// console.log(sessionStorage.getItem('name'))
+// sessionStorage.setItem('name','shubham')
+// console.log(sessionStorage.getItem('name'))
+
+
+// // cookies
+// document.cookie = 'name=pratik; expires=' + new Date(2030,5,3).toUTCString()
+// console.log(document.cookie)
+
+
